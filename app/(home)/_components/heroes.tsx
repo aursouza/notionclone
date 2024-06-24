ac3f29130ed8a806f1d04ad7ee510c8494fe83f2
Marco Souza <aursouza28@gmail.com>
2024-06-23 22:34:27 -0300

    fix: fix images to theme dark

--- app/(home)/_components/heroes.tsx
+++ app/(home)/_components/heroes.tsx
@@ -9,7 +9,13 @@ export const Heroes = () => {
             src="/documents.png"
             alt="Documents"
             fill
-            className="object-contain"
+            className="object-contain dark:hidden"
+          />
+          <Image
+            src="/documents-dark.png"
+            alt="Documents"
+            fill
+            className="object-contain light:hidden"
           />
         </div>
         <div className="relative h-[400px] w-[400px] hidden md:block">
@@ -17,7 +23,13 @@ export const Heroes = () => {
             src="/reading.png"
             alt="Reading"
             fill
-            className="object-contain"
+            className="object-contain dark:hidden"
+          />
+          <Image
+            src="/reading-dark.png"
+            alt="Reading"
+            fill
+            className="object-contain light:hidden"
           />
         </div>
       </div>
--- app/(home)/_components/logo.tsx
+++ app/(home)/_components/logo.tsx
@@ -10,7 +10,20 @@ const font = Poppins({
 export const Logo = () => {
   return (
     <div className="hidden md:flex items-center gap-x-2">
-      <Image src="/logo.svg" alt="Logo" height="40" width="40" />
+      <Image
+        src="/logo.svg"
+        alt="Logo"
+        height="40"
+        width="40"
+        className="dark:hidden"
+      />
+      <Image
+        src="/logo-dark.svg"
+        alt="Logo"
+        height="40"
+        width="40"
+        className="light:hidden"
+      />
       <p className={cn('font-semibold', font.className)}>MeuNotion</p>
     </div>
   )
