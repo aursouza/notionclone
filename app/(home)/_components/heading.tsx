4ba1441edf3d57b7ffaf65fab5f8e0f8b451261d
Marco Souza <aursouza28@gmail.com>
2024-06-23 22:12:43 -0300

    fix: alter text from heading

--- app/(home)/_components/heading.tsx
+++ app/(home)/_components/heading.tsx
@@ -7,7 +7,7 @@ export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-w-3xl sm:text-5xl md:text-6xl font-bold">
-        Suas Ideias. Documentos e Planos Unificados. Bem vindo ao{' '}
+        Suas Ideias, Documentos e Planos unificados. Bem vindo ao{' '}
         <span className="underline">MeuNotion</span>
       </h1>
       <h3 className=" text-base sm:text-xl md:text-2xl font-medium">
